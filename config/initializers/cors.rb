022b777b8a3c0cde39751cd579104f460a0fcd42
Fabiano Leite <fabianoleittes@gmail.com>
2021-02-11 17:21:33 -0300

    chore: update rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(ENV.fetch('ALLOW_REQUESTS_FROM', Rails.application.config.hosts).split(','))
+    origins(*)
 
     resource '*',
       headers: :any,
