8626d24938a74d7f674bab3ea3cba9a9e112b814
Fabiano Leite <fabianoleittes@gmail.com>
2021-02-11 13:30:26 -0300

    chore: add load dotenv at the rails bootstrap

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_view/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module CodeChallengeLeveeRuby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
